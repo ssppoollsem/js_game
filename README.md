95c29780d3e0ca8230c79219907f63a96e237b1b
ITEMBAY\jjseo <jjseo@itembay.com>
2022-05-27 10:17:27 +0900

    오타 수정

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # js_game
+
+게임을 만들며 Javascript에 익숙해지는 것이 목표입니다.
--- game1/coding/code/js/class.js
+++ game1/coding/code/js/class.js
@@ -4,21 +4,20 @@ class Hero {
     // 생성자
     constructor(el) {
         this.el = document.querySelector(el);
-        console.log(this.el)
+        // console.log(this.el)
     }
 
     // 히어로의 움직임 변경
     keyMotion() {
         // 좌측, 우측 방향키
         if(key.keyDown['left']) {
-            this.el.classList.add('run');
-            this.el.classList.add('flip');
+            this.el.classList.add('run', 'flip');
         }else if(key.keyDown['right']) {
             this.el.classList.add('run');
             this.el.classList.remove('flip');
         }
 
-        // 곻격키
+        // 공격키
         if(key.keyDown['attack']) {
             this.el.classList.add('attack')
         }
